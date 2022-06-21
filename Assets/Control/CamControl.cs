02f135cd53e6fcea5101f677998425b55c1ae7f6
theProdWorm <emil.rohlen@live.se>
2022-06-21 19:04:24 +0200

    basic zoom

--- Assets/Control/CamControl.cs
+++ Assets/Control/CamControl.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 
 public class CamControl : MonoBehaviour {
-    public float speed;
+    public float panSpeed;
+    public float zoomSpeed;
+
+    private Camera cam;
+
+    private void Start ( ) {
+        cam = GetComponent<Camera>( );
+
+        transform.position -= transform.forward * 100;
+    }
 
     private void Update ( ) {
+        PanKB( );
+        Zoom( );
+    }
+
+    private void PanKB ( ) {
         Vector2 move = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
         if (Mathf.Abs(move.x) == Mathf.Abs(move.y)) move *= Mathf.Cos(Mathf.PI / 4);
 
-        transform.position += move.x * speed * Time.deltaTime * transform.right;
-        transform.position += move.y * speed * Time.deltaTime * new Vector3(1, 0, 1);
+        transform.position += move.x * panSpeed * Time.deltaTime * transform.right;
+        transform.position += move.y * panSpeed * Time.deltaTime * new Vector3(1, 0, 1);
+    }
+
+    private void Zoom ( ) {
+        cam.orthographicSize -= zoomSpeed * Time.deltaTime * Input.mouseScrollDelta.y;
     }
 }
