a6e55e6b884848d237b1b21875958cf0faacf151
theProdWorm <emil.rohlen@live.se>
2022-06-21 18:53:40 +0200

    pan using WASD/arrow keys

--- Assets/Control/CamControl.cs
+++ Assets/Control/CamControl.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 
 public class CamControl : MonoBehaviour {
-    void Start ( ) {
+    public float speed;
 
-    }
+    private void Update ( ) {
+        Vector2 move = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-    void Update ( ) {
+        if (Mathf.Abs(move.x) == Mathf.Abs(move.y)) move *= Mathf.Cos(Mathf.PI / 4);
 
+        transform.position += move.x * speed * Time.deltaTime * transform.right;
+        transform.position += move.y * speed * Time.deltaTime * new Vector3(1, 0, 1);
     }
 }
