ba93aabb69622a1e7d958792c8d01c0b1f253d66
theProdWorm <emil.rohlen@live.se>
2022-06-22 18:06:44 +0200

    removed hardcoded stats

--- Assets/Scripts/Enemies/Goblin.cs
+++ Assets/Scripts/Enemies/Goblin.cs
@@ -1,12 +1,6 @@
 ﻿using UnityEngine;
 
 public class Goblin : Enemy {
-    public Goblin ( ) {
-        speed = 2;
-        hp = 2;
-        damage = 1;
-    }
-
     protected override void Update ( ) {
         base.Update( );
     }
--- Assets/Scripts/Enemies/Orc.cs
+++ Assets/Scripts/Enemies/Orc.cs
@@ -1,12 +1,6 @@
 ﻿using UnityEngine;
 
 public class Orc : Enemy {
-    public Orc ( ) {
-        speed = 1;
-        hp = 10;
-        damage = 5;
-    }
-
     protected override void Update ( ) {
         base.Update( );
     }
