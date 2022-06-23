5da969f826315685259138e7fdc7595751dfaebd
theProdWorm <emil.rohlen@live.se>
2022-06-23 14:39:02 +0200

    land tile pulsing highlight glow

--- Assets/Scripts/Level/LandTile.cs
+++ Assets/Scripts/Level/LandTile.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 
 public class LandTile : MonoBehaviour {
-    public float maxScale;
+    public float maxGlow;
+    public float minGlow;
+    public float glowCycle;
 
     private new MeshRenderer renderer;
     private Color defColor;
+
+    private bool isGlowing;
+    private float glowStage;
 
     private TowerCollection towerCollection;
 
@@ -18,12 +23,31 @@ public class LandTile : MonoBehaviour {
         defColor = renderer.material.color;
     }
 
+    private void Update ( ) {
+        if (isGlowing) {
+            if (glowStage >= Mathf.PI * 2)
+                glowStage -= Mathf.PI * 2;
+            glowStage += Time.deltaTime * (1 / glowCycle);
+
+            float lum = minGlow + Mathf.Abs(Mathf.Sin(glowStage)) * (maxGlow - minGlow);
+
+            renderer.material.SetColor("_EmissionColor", Color.yellow * lum);
+        }
+    }
+
     private void OnMouseEnter ( ) {
-        renderer.material.color = Color.yellow;
+        //renderer.material.color = Color.yellow;
+
+        renderer.material.EnableKeyword("_EMISSION");
+        isGlowing = true;
     }
 
     private void OnMouseExit ( ) {
-        renderer.material.color = defColor;
+        //renderer.material.color = defColor;
+
+        renderer.material.DisableKeyword("_EMISSION");
+        isGlowing = false;
+        glowStage = 0;
     }
 
     private void OnMouseUpAsButton ( ) {
@@ -32,5 +56,7 @@ public class LandTile : MonoBehaviour {
         if (transform.childCount > 0) return;
 
         var twr = Instantiate(towerCollection.towers[0], transform);
+
+        renderer.material.color = defColor;
     }
 }
