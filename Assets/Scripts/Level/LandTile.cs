192c5598f0b4ddd6413dc2ab101614109bef93ef
theProdWorm <emil.rohlen@live.se>
2022-06-23 13:38:46 +0200

    land tile highlight

--- Assets/Scripts/Level/LandTile.cs
+++ Assets/Scripts/Level/LandTile.cs
@@ -3,20 +3,33 @@
 public class LandTile : MonoBehaviour {
     public float maxScale;
 
+    private new MeshRenderer renderer;
+    private Color defColor;
+
     private TowerCollection towerCollection;
 
     private void Awake ( ) {
         towerCollection = FindObjectOfType<TowerCollection>( );
     }
 
+    private void Start ( ) {
+        renderer = GetComponent<MeshRenderer>( );
+
+        defColor = renderer.material.color;
+    }
+
     private void OnMouseEnter ( ) {
-        transform.localScale = new Vector3(1, maxScale, 1);
-        transform.position += Vector3.up * ((maxScale - 1) / 2);
+        //transform.localScale = new Vector3(1, maxScale, 1);
+        //transform.position += Vector3.up * ((maxScale - 1) / 2);
+
+        renderer.material.color = Color.yellow;
     }
 
     private void OnMouseExit ( ) {
-        transform.localScale = Vector3.one;
-        transform.position -= Vector3.up * ((maxScale - 1) / 2);
+        //transform.localScale = Vector3.one;
+        //transform.position -= Vector3.up * ((maxScale - 1) / 2);
+
+        renderer.material.color = defColor;
     }
 
     private void OnMouseUpAsButton ( ) {
